c72ae7b40eead592d9f8ce7a3d793888a1e3ddc1
malvis650 <chiatohmalvis650@gmail.com>
2025-06-10 12:26:50 +0100

    first commit

---
+++ src/Services.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+      <h1 style = {{color: "crimson"}}>Our services</h1>
+    </div>
+  )
+}
+
+export default Services
